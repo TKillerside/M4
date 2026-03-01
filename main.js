fda4ddb64b35546b34fc890ae2aebbb99a7ec92e
t.killerside@gmail.com <t.killerside@gmail.com>
2026-03-01 13:24:24 +0300

    finish

---
+++ main.js
@@ -0,0 +1,78 @@
+let start = document.getElementById("start");
+let result = document.getElementById("result");
+let main = document.getElementById("main");
+
+let current = 0;
+let count = 0;
+let questions = [
+    {
+        question: "2+2",
+        correct: 4,
+        answers: [4, 5, 10, 12, 33],
+    },
+    {
+        question: "4+4",
+        correct: 8,
+        answers: [1, 5, 8, 33],
+    },
+        {
+        question: "10-9",
+        correct: 1,
+        answers: [2, 1, 5, -3],
+    },
+        {
+        question: "22/2",
+        correct: 11,
+        answers: [10, 8, 11, 2],
+    },
+            {
+        question: "9*9",
+        correct: 81,
+        answers: [90, 9, 81, 99],
+    },
+];
+
+function startProgram() {
+    start.classList.add("close");
+    main.classList.remove("close");
+    result.classList.add("close");
+    generate();
+}
+
+start.addEventListener("click", startProgram);
+
+function generate() {
+    let question = questions[current].question;
+    main.innerHTML = `<h2 class="title">${question}</h2>`;
+
+    let answers = questions[current].answers;
+    let block = "";
+    for (let i of answers) {
+        block += ` <button class="btn" onclick="check('${i}')">${i}</button>`;
+    }
+
+    main.innerHTML += `<nav>${block}</nav>`;
+}
+
+function check(answer) {
+    correct = questions[current].correct;
+    if (correct == answer) {
+        count += 1;
+    }
+    current += 1;
+    if (questions.length > current) {
+        generate()
+    } else {
+        stop()
+    }
+}
+
+function stop(){
+    start.classList.remove("close");
+    main.classList.add("close");
+    result.classList.remove("close");
+
+    result.innerHTML = `Решено ${count} из ${questions.length}`;
+    current = 0;
+    count = 0;
+}
