3ccb9833cab6941c5bd454086d661fbd78092085
t.killerside@gmail.com <t.killerside@gmail.com>
2026-04-19 13:24:32 +0300

    paint

--- carusel/main.js
+++ carusel/main.js
@@ -1,20 +1,33 @@
-let btn_left = document.getElementById("btn_left")
-let btn_right = document.getElementById("btn_right")
-let carusel = document.getElementById("carusel")
+let btn_left = document.getElementById("btn_left");
+let btn_right = document.getElementById("btn_right");
+let carusel = document.getElementById("carusel");
 
-let carusel_elem = 3
-let width = 100 / carusel_elem
+let carusel_elem = 3;
+let width = 100 / carusel_elem;
 
 for (let i of carusel.children) {
-    i.style.minWidth = `${width}%`
+    i.style.minWidth = `${width}%`;
 }
 
-let count_item = carusel.children.length
-let current = 0
+let count_item = carusel.children.length;
+let current = 0;
 
 btn_right.addEventListener("click", Right);
 
 function Right() {
-    current += 1
+    current += 1;
+    if (current > count_item) {
+        current = 0;
+    }
+    carusel.style.transform = `translateX(-${width * current}%)`;
+}
+
+btn_left.addEventListener("click", Left);
+
+function Left() {
+    current -= 1;
+    if (current < 0) {
+        current = count_item;
+    }
     carusel.style.transform = `translateX(-${width * current}%)`;
 }
